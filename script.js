649273456ad645a9315607bb3abe92b825582edb
Kristina Maier <kaudiking@gmail.com>
2021-11-02 14:33:00 +0100

    mensaje2323

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ $('#submit').on("click", function() {
 	
 	$('#taskList').html("Has hecho click!");
 
+
+   // function nombreDeLaFuncion(parametro1) {
+        // ...
+      //  return resultado;
+    //}
+
+
+    function decirHola() {
+        console.log("Hola!");
+    }
+    
+    // "cuando alguien haga click en el elemento con la clase tarjeta,
+    // tenemos que llamar a la función decirHola"
+   // $('.tarjeta').on("click", decirHola);
+
+
+// "cuando alguien haga click en el elemento con la clase tarjeta,
+// tenemos que llamar a la función anónima que escribimos aquí"
+$('.tarjeta').on("click", function() {
+	console.log("Hola!");
+});
+
+         
+
+               
+
 })
 
 
