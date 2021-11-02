65491b988428612b8e1d52e6a9e83fff7eebd9bb
Kristina Maier <kaudiking@gmail.com>
2021-11-02 14:57:09 +0100

    sdsd

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-// console.log("Hello! I am an alert box!!");
-
 $('#submit').on("click", function() {
+	let newTask = $('#newTask').val();
 	
-	$('#taskList').html("Has hecho click!");
+	tasks.push(newTask);
 
-
-   // function nombreDeLaFuncion(parametro1) {
-        // ...
-      //  return resultado;
-    //}
-
-
-    function decirHola() {
-        console.log("Hola!");
-    }
-    
-    // "cuando alguien haga click en el elemento con la clase tarjeta,
-    // tenemos que llamar a la función decirHola"
-   // $('.tarjeta').on("click", decirHola);
-
-
-// "cuando alguien haga click en el elemento con la clase tarjeta,
-// tenemos que llamar a la función anónima que escribimos aquí"
-$('.tarjeta').on("click", function() {
-	console.log("Hola!");
-});
-
-         
-
-               
-
+	console.log(`Tenemos las siguientes tareas en la lista: ${tasks}`);
+	
 })
+
+
 
 
